405f58d85a79c279517cecb575e3a067acb40bdc
Kevin Miller <kmillergit@outlook.com>
2020-08-22 07:02:03 -0700

    fixed employees showing as undefined

--- app.js
+++ app.js
@@ -96,7 +96,7 @@ async function main() {
         const managerAnswers = await inquirer.prompt(managerQuestions);
         console.log(managerAnswers);
         // create a new Manager obj and push into the employees array, to be used in renderer function
-        let managerEmployee = new Manager(managerAnswers.name, managerAnswers.role, managerAnswers.email, managerAnswers.id, managerAnswers.officeNumber);
+        let managerEmployee = new Manager(managerAnswers.name, managerAnswers.id, managerAnswers.email, managerAnswers.officeNumber, managerAnswers.role,);
         employees.push(managerEmployee);
         console.log(employees);
 
@@ -115,13 +115,13 @@ async function main() {
                 const internAnswer = await inquirer.prompt(internQuestions);
                 console.log(internAnswer);
                 // create a new employee obj using the appropriate class
-                let internEmployee = new Intern(internAnswer.name, internAnswer.role, internAnswer.email, internAnswer.id, internAnswer.school);
+                let internEmployee = new Intern(employeeAnswers.name, employeeAnswers.id, employeeAnswers.email, internAnswer.school, internAnswer.role,);
                 employees.push(internEmployee);
             }
             else {
                 const engineerAnswer = await inquirer.prompt(engineerQuestions);
                 console.log(engineerAnswer);
-                let engiEmployee = new Engineer(engineerAnswer.name, engineerAnswer.role, engineerAnswer.email, engineerAnswer.id, engineerAnswer.github);
+                let engiEmployee = new Engineer(employeeAnswers.name, employeeAnswers.id, employeeAnswers.email, engineerAnswer.github, engineerAnswer.role,);
                 employees.push(engiEmployee);
             }
 
@@ -132,6 +132,7 @@ async function main() {
                 break;
             }
         }
+        console.log(employees);
         let teamHtml = render(employees);
 
         fs.writeFile("./output/team.html", teamHtml, function(error) {
